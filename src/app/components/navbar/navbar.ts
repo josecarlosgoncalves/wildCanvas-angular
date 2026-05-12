85e85d8c85ecd6448c35955528e33a89742538c9
jose carlos goncalves <josecarlosgoncalves@outlook.pt>
2026-05-12 21:18:39 +0100

    hamburger bar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,4 +7,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update((isOpen) => !isOpen);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
